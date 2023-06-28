a19a7d574fe8ef34691fb3070cb124e1f769f7d1
adocxwork <adipracc@gmail.com>
2023-06-28 19:25:53 +0530

    files added..

---
+++ LearningPrograms/m5_java_objects_and_methods/p0_Temp.java
@@ -0,0 +1,23 @@
+class MyClass {
+
+    int x;
+
+    MyClass() {
+
+        x = 5;
+
+    }
+
+}
+
+public class p0_Temp {
+
+    public static void main(String[] args) {
+
+        MyClass obj = new MyClass();
+
+        System.out.println(obj.x);
+
+    }
+
+}
